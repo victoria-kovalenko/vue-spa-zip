ae83991fe35a2b5242bb4962ec0aa316748e32fd
Victoria Kovalenko <v.kovalenkoooooo@gmail.com>
2023-03-23 17:23:22 +0200

    deploy #2

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,69 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1> ZIP code</h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <div 
+    class="input-group mb-3 input-group--all"
+    >
+      <div class="input-group--title">
+        <h2 class="input-group--title_style"> Find the info by ZIP code</h2>
+      </div>
+
+      <div class="input-group--form">
+        <input 
+          type="text" 
+          class="input-area" 
+          placeholder="Please enter ZIP code" 
+          aria-label="Please enter ZIP code" 
+          aria-describedby="button-addon2">
+
+        <button 
+          class="btn btn-outline-secondary" 
+          type="button" 
+          id="button-addon2">
+          Search
+        </button>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
+.input-group--all {
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%);
+  width: 400px;
+  border: solid black 0.5px;
+  border-radius: 20px;
+}
+
+.input-area {
+  display: block;
+}
+
+.input-group--title {
+  background-color: aquamarine;
+  border-radius: 20px;
+  /* p-bottom: 10px; */
+}
+
+.input-group--title_style {
+  display: inline-block;
+  border-radius: 20px;
+}
+
+.input-group--form {
+  display: flex;
+  justify-content: center;
+}
+/* header {
   line-height: 1.5;
 }
 
@@ -43,5 +92,5 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
-}
+} */
 </style>

f41b583f63b5d006c24c6a9b257aedee5936397a
Victoria Kovalenko <v.kovalenkoooooo@gmail.com>
2023-03-26 18:42:59 +0300

    Update Card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -8,7 +8,7 @@
       <div class="card-body">
         <h5 class="card-title">Special title treatment</h5>
         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="/" class="btn btn-primary">Go back to main</a>
+        <a href="#" class="btn btn-primary">Go back to main</a>
       </div>
     </div>
 </template>
